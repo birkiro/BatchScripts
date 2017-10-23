ca43a161ec69a0585126036ff8cd1bb8c16fc916
Birkir Oskarsson <birkiro@gmail.com>
2017-10-23 09:14:16 +0200

    Update[GithubPull]: Made it smarter.

--- Git/Github Pull.bat
+++ Git/Github Pull.bat
@@ -1,10 +1,34 @@
 @echo off
 
-SET GITPATH=%1
-echo %GITPATH%
+if "%1." == "." goto not_specifed
 
-cd %GITPATH%
-git pull
+set PROJECT=%~1
+set PROJECTDIR=C:\Github
 
-pause
+if %PROJECT%==ALL goto pull_all
+call :pull %PROJECT%
+goto finished
+
+:pull_all
+echo Pulling all projects
+call :pull Patterns
+call :pull BatchScripts
+echo Finished pulling all projects from the Github repository
+goto finished
+
+:pull
+cd %PROJECTDIR%\%PROJECT%
+if %errorLevel% == 0 (
+	echo Git Pulling %PROJECT%
+	git pull
+) else (
+	echo Project path to '%PROJECT%' does not exist.
+)
 goto :eof
+
+:not_specified
+echo Command line parameter missing.
+echo available parameters: [ALL], [Patterns], [BatchScripts].
+
+:finished
+pause
